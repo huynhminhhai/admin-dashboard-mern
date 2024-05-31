dda07fe22d7165691dba6ef07f872a83444e7bc5
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-05-31 14:07:35 +0700

    [UPDATE] validator for get User by id

--- src/routes/general.routes.ts
+++ src/routes/general.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { getUserController } from '~/controllers/general.controller'
+import { getUserValidator } from '~/middlewares/general.middleware'
 import { wrapRequestHandler } from '~/utils/handler'
 
 const generalRoutes = Router()
 
-generalRoutes.get('/user/:id', wrapRequestHandler(getUserController))
+generalRoutes.get('/user/:id', getUserValidator, wrapRequestHandler(getUserController))
 
 export default generalRoutes
